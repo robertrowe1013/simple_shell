39e08b51a0f4e5f5f92961a55c2036c6e600dd69
robertrowe1013 <robertrowe1013@gmail.com>
2020-04-10 22:49:22 +0000

    moved builtins to builtins.c

---
+++ builtins.c
@@ -0,0 +1,48 @@
+#include "vrshell.h"
+/**
+  * checkbi - check if argument is a builtin
+  * @tokens: tokens from parsed string
+  *
+  * Return: 0 if builtin
+  */
+int checkbi(char **tokens)
+{
+	if (_strncmp(tokens[0], "env", 3) == 0)
+		return (0);
+	if (_strncmp(tokens[0], "exit", 4) == 0)
+		return (0);
+	return (-1);
+}
+/**
+  * runbi - run builtin
+  * @tokens: tokens from parsed string
+  *
+  * Return: 1 if successful
+  */
+int runbi(char **tokens)
+{
+	int i = 0;
+	int i2 = 0;
+
+	if (_strncmp(tokens[0], "env", 3) == 0)
+	{
+		while (environ[i] != NULL)
+		{
+			while (environ[i][i2] != '\0')
+			{
+				_putchar(environ[i][i2]);
+				i2++;
+			}
+			_putchar('\n');
+			i++;
+			i2 = 0;
+		}
+	}
+	else if (_strncmp(tokens[0], "exit", 4) == 0)
+	{
+		/* FREE TOKENS HERE */
+		write(STDOUT_FILENO, "logout\n", 8);
+		exit(0);
+	}
+	return (1);
+}
--- vrsh.c
+++ vrsh.c
@@ -11,35 +11,22 @@ int main(void)
 	char *prompt = "$ ";
 	char *arg;
 	char **tokens;
-	int i = 0;
-	int j = 0, wc = 0;
+	int wc = 0;
+	int bi;
 
 	while (status)
 	{
 		write(STDOUT_FILENO, prompt, 2);
 		arg = read_line();
-		if (_strncmp(arg, "env", 3) == 0)
-		{
-			while (environ[i] != NULL)
-			{
-				while (environ[i][j] != '\0')
-				{
-					_putchar(environ[i][j]);
-					j++;
-				}
-				_putchar('\n');
-				i++;
-				j = 0;
-			}
-		}
+		wc = count_words(arg, " \n");
+		tokens = parse_str(arg, " \n", wc);
+		bi = checkbi(tokens);
+		if (bi == 0)
+			status = runbi(tokens);
 		else
-		{
-			wc = count_words(arg, " \n");
-			tokens = parse_str(arg, " \n", wc);
 			status = executearg(tokens);
-			free(tokens);
-			/*free(arg);*/
-		}
+		free(tokens);
+		/*free(arg);*/
 	}
 	return (0);
 }
